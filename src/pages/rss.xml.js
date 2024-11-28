244dbaaa04acee1370b0df630f3219b95c2eb4f5
sarakhanX <sarawut.khan@hotmail.com>
2024-11-28 14:52:40 +0700

    fixed

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,16 +1,18 @@
 import rss from '@astrojs/rss';
+import { getCollection } from 'astro:content';
 
 export async function GET(context) {
-  const blog = await context.glob('./blog/*.{md,mdx}');
+  const posts = await Astro.glob('./blog/*.{md,mdx}');
   return rss({
     title: 'Your Blog Title',
     description: 'Your blog description',
     site: context.site,
-    items: blog.map((post) => ({
+    items: posts.map((post) => ({
       title: post.frontmatter.title,
       pubDate: new Date(post.frontmatter.date),
       description: post.frontmatter.description,
-      link: post.url,
+      link: `/blog/${post.file.split('/').pop().split('.')[0]}/`,
     })),
+    customData: `<language>en-us</language>`,
   });
 }
